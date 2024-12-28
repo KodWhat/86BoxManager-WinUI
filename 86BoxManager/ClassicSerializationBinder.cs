a6e1350590482269836fef426e19634834725fc1
Flavien Lefebvre <flavien-lefebvre@hotmail.fr>
2024-12-28 17:27:21 +0100

    Enable nullables

--- 86BoxManager/ClassicSerializationBinder.cs
+++ 86BoxManager/ClassicSerializationBinder.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace EightySixBoxManager;
 internal class ClassicSerializationBinder : SerializationBinder
 {
-	public override Type BindToType(string assemblyName, string typeName)
+	public override Type? BindToType(string assemblyName, string typeName)
 	{
 		if (assemblyName.StartsWith("86Manager") && typeName == "_86boxManager.VM")
 		{
